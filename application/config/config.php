a643f845f344616176608a5c892cb3e0059e3a60
Palacios <jfdominguezpalacios@gmail.com>
2014-09-14 22:14:07 +0200

    dnhgnfghng

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'localhost/covisan';
+$config['base_url']	= 'http://www.bodegascovisan.com';
 
 /*
 |--------------------------------------------------------------------------
